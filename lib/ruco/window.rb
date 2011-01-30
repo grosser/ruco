b79a48b5565e4d8bab50803b094757b7aa88ef93
grosser <grosser.michael@gmail.com>
2011-01-30 12:49:39 +0100

    top/left scrolling for window

--- lib/ruco/window.rb
+++ lib/ruco/window.rb
@@ -1,8 +1,15 @@
 module Ruco
   class Window
+    OFFSET = 5
+
+    attr_writer :position
+    attr_reader :top, :left
+
     def initialize(lines, columns)
       @lines = lines
       @columns = columns
+      @top = 0
+      @left = 0
     end
 
     def crop(content)
@@ -13,6 +20,21 @@ module Ruco
         line.slice!(@columns, 99999) # drop everything we dont need
         line + (' ' * (@columns - line.size))
       end
+    end
+
+    def position=(pos)
+      @top = pos.line - OFFSET unless visible_lines.include?(pos.line)
+      @left = pos.column - OFFSET  unless visible_columns.include?(pos.column)
+    end
+
+    private
+
+    def visible_lines
+      @top..(@top+@lines-1)
+    end
+
+    def visible_columns
+      @left..(@left+@columns-1)
     end
   end
 end
--- spec/ruco/window_spec.rb
+++ spec/ruco/window_spec.rb
@@ -1,8 +1,15 @@
 require File.expand_path('spec/spec_helper')
 
 describe Ruco::Window do
+
   describe :crop do
     let(:window){ Ruco::Window.new(2,4) }
+
+    it "does not modify given lines" do
+      original = ['1234','1234']
+      window.crop(original)
+      original.should == ['1234','1234']
+    end
 
     it "removes un-displayable chars" do
       result = window.crop(['12345','12345','12345'])
@@ -17,6 +24,85 @@ describe Ruco::Window do
     it "creates lines if necessary" do
       result = window.crop(['1234'])
       result.should == ['1234','    ']
+    end
+
+    it "stays inside frame as long as position is in frame" do
+      window.position = Ruco::Position.new(1,3)
+      result = window.crop(['12345678','12345678'])
+      result.should == ['1234','1234']
+    end
+
+#    it "goes out of frame if position is out of frame" do
+#      window.position = Ruco::Position.new(1,4)
+#      result = window.crop(['1234567890','1234567890'])
+#      result.should == ['5678','5678']
+#    end
+#
+#    it "goes far out of frame" do
+#      window.position = Ruco::Position.new(1,8)
+#      result = window.crop(['1234567890','1234567890'])
+#      result.should == ['5678','5678']
+#    end
+  end
+
+  describe :top do
+    let(:window){ Ruco::Window.new(10,10) }
+
+    it "does not change when staying in frame" do
+      window.top.should == 0
+      window.position = Ruco::Position.new(9,0)
+      window.top.should == 0
+    end
+
+    it "changes by offset when going vertically out of frame" do
+      window.position = Ruco::Position.new(10,0)
+      window.top.should == 5
+    end
+
+    it "changes to x - offset when going down out of frame" do
+      window.position = Ruco::Position.new(20,0)
+      window.top.should == 15
+    end
+
+    it "changes to x - offset when going down out of frame" do
+      window.position = Ruco::Position.new(20,0)
+      window.position = Ruco::Position.new(7,0)
+      window.top.should == 2
+    end
+  end
+
+  describe :left do
+    let(:window){ Ruco::Window.new(10,10) }
+
+    it "does not change when staying in frame" do
+      window.left.should == 0
+      window.position = Ruco::Position.new(0,9)
+      window.left.should == 0
+    end
+
+    it "changes by offset when going vertically out of frame" do
+      window.position = Ruco::Position.new(0,9)
+      window.position = Ruco::Position.new(0,10)
+      window.left.should == 5
+    end
+
+    it "changes to x - offset when going right out of frame" do
+      window.position = Ruco::Position.new(0,20)
+      window.left.should == 15
+    end
+
+    it "changes to x - offset when going left out of frame" do
+      window.position = Ruco::Position.new(0,20)
+      window.position = Ruco::Position.new(0,7)
+      window.left.should == 2
+    end
+
+    it "does not change when staying in changed frame" do
+      window.position = Ruco::Position.new(0,9)
+      window.position = Ruco::Position.new(0,10)
+      window.left.should == 5
+      window.position = Ruco::Position.new(0,14)
+      window.left.should == 5
     end
   end
 end
