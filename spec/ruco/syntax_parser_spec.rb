19cf0944a5c8704b0f6763d34d4a9caa51ec31ef
grosser <grosser.michael@gmail.com>
2011-09-14 20:21:24 +0200

    fix specs

--- spec/ruco/syntax_parser_spec.rb
+++ spec/ruco/syntax_parser_spec.rb
@@ -11,28 +11,42 @@ describe Ruco::SyntaxParser do
 
   describe :parse_lines do
     it "shows positions for simple lines" do
-      parse(["class Foo","end"]).should == [
-        [[:constant, 6...9], [:keyword, 0...5]],
-        [[:keyword, 0...3]]
-      ]
+      parse(["class Foo","end"]).should == [[
+        ["keyword.control.class.ruby", 0...5],
+        ["entity.name.type.class.ruby", 6...9],
+        ["meta.class.ruby", 0...9]],
+        [["keyword.control.ruby", 0...3]
+      ]]
     end
 
     it "shows positions for complicated lines" do
-      parse(["class foo end blob else Foo"]).should == [
-        [[:constant, 24...27], [:keyword, 0...5], [:keyword, 10...13], [:keyword, 19...23]]
-      ]
+      parse(["class foo end blob else Foo"]).should == [[
+        ["keyword.control.class.ruby", 0...5],
+        ["entity.name.type.class.ruby", 6...9],
+        ["meta.class.ruby", 0...9],
+        ["keyword.control.ruby", 10...13],
+        ["keyword.control.ruby", 19...23],
+        ["variable.other.constant.ruby", 24...27]
+      ]]
     end
 
     it "does not show keywords in strings" do
-      parse(["Bar 'Bar' foo"]).should == [
-        [[:string, 4...9], [:constant, 0...3]]
-      ]
+      parse(["Bar 'Bar' foo"]).should == [[
+        ["variable.other.constant.ruby", 0...3],
+        ["punctuation.definition.string.begin.ruby", 4...5],
+        ["punctuation.definition.string.end.ruby", 8...9],
+        ["string.quoted.single.ruby", 4...9]
+      ]]
     end
 
     it "does not show strings in comments" do
-      parse(["'Bar' # 'Bar'"]).should == [
-        [[:comment, 6...13], [:string, 0...5]]
-      ]
+      parse(["'Bar' # 'Bar'"]).should == [[
+        ["punctuation.definition.string.begin.ruby", 0...1],
+        ["punctuation.definition.string.end.ruby", 4...5],
+        ["string.quoted.single.ruby", 0...5],
+        ["punctuation.definition.comment.ruby", 6...7],
+        ["comment.line.number-sign.ruby", 6...13]
+      ]]
     end
   end
 end
