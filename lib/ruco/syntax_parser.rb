fd16e7640f064a8d69b1fe8313ed5b1d926a51fb
grosser <grosser.michael@gmail.com>
2011-09-17 07:46:02 +0200

    cache syntax nodes

--- lib/ruco/syntax_parser.rb
+++ lib/ruco/syntax_parser.rb
@@ -1,12 +1,19 @@
 module Ruco
   module SyntaxParser
     def self.parse_lines(lines, language)
-      syntax = File.join(Uv.syntax_path, "#{language}.syntax")
-      if syntax = Textpow::SyntaxNode.load(syntax)
+      if syntax = syntax_node(language)
         processor = syntax.parse(lines.join("\n"),  Ruco::ArrayProcessor.new)
         processor.lines
       else
         []
+      end
+    end
+
+    def self.syntax_node(language)
+      @@syntax_node ||= {}
+      @@syntax_node[language] ||= begin
+        syntax = File.join(Uv.syntax_path, "#{language}.syntax")
+        Textpow::SyntaxNode.load(syntax)
       end
     end
   end
